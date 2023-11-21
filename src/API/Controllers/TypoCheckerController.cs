5636574035b8ae1ebeb569366e4c5c61dd6bbdbf
ShadowD1337 <martin.jechkov@gmail.com>
2023-11-21 21:26:09 +0200

    Started Work On Endpoint

---
+++ src/API/Controllers/TypoCheckerController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using Azure;
+using Azure.AI.FormRecognizer.DocumentAnalysis;
+using System.Reflection.Metadata;
+using System.Text;
+using System.IO;
+using API.Settings;
+using Microsoft.Extensions.Options;
+
+namespace API.Controllers
+{
+    [ApiController, Route("api")]
+    public class TypoCheckerController : ControllerBase
+    {
+        private readonly DocumentIntelligenceSettings _settings;
+        public TypoCheckerController(IOptions<DocumentIntelligenceSettings> options)
+        {
+            this._settings = options.Value ?? throw new InvalidOperationException("DocumentIntelligenceSettings must be configured!");
+        }
+
+        [HttpPost("check")]
+        public async Task<IActionResult> CheckAsync()
+        {
+            var endpoint = "https://typochecker-ocr.cognitiveservices.azure.com/";
+            var apiKey = this._settings.APIKey;
+            var credential = new AzureKeyCredential(apiKey);
+            var client = new DocumentAnalysisClient(new Uri(endpoint), credential);
+
+            foreach (var file in this.Request.Form.Files)
+            {
+                using var stream = file.OpenReadStream();
+                var operation = await client.AnalyzeDocumentAsync(WaitUntil.Completed, "prebuilt-read", stream, new AnalyzeDocumentOptions { Locale = "bg" });
+            }
+
+            return this.Ok();
+        }
+    }
+}
--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,4 +1,6 @@
 
+using API.Settings;
+
 namespace API
 {
     public class Program
@@ -8,6 +10,8 @@ namespace API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Configuration.AddJsonFile("appsettings.local.json", optional: true);
+            builder.Services.Configure<DocumentIntelligenceSettings>(builder.Configuration.GetSection(DocumentIntelligenceSettings.Section));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
