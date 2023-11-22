6064260a8ddc1184353f8878e80978664bd332bb
ShadowD1337 <121127640+ShadowD1337@users.noreply.github.com>
2023-11-22 16:48:33 +0200

    Make Changes

--- src/API/Controllers/TypoCheckerController.cs
+++ src/API/Controllers/TypoCheckerController.cs
@@ -1,19 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Text;
+﻿using API.Features;
+using API.Settings;
 using Azure;
 using Azure.AI.FormRecognizer.DocumentAnalysis;
-using System.Reflection.Metadata;
-using System.Text;
-using System.IO;
-using API.Settings;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using API.Features;
+using SkiaSharp;
 
 namespace API.Controllers
 {
     [ApiController, Route("api")]
     public class TypoCheckerController : ControllerBase
     {
+        private static readonly char[] _delimiters = ".,!?:'\"()".ToCharArray();
+
         private readonly DocumentIntelligenceSettings _settings;
         private readonly IWordsRegister _wordsRegister;
         public TypoCheckerController(IWordsRegister wordsRegister, IOptions<DocumentIntelligenceSettings> options)
@@ -33,20 +32,40 @@ namespace API.Controllers
 
             foreach (var file in this.Request.Form.Files)
             {
-                using var stream = file.OpenReadStream();
-                var operation = await client.AnalyzeDocumentAsync(WaitUntil.Completed, "prebuilt-read", stream, new AnalyzeDocumentOptions { Locale = "bg" }, cancellationToken);
+                var pathToFile = await SaveImageAsync("data", this.HttpContext.TraceIdentifier, file, cancellationToken);
+
+                await using var fileStream = new FileStream(pathToFile, FileMode.Open, FileAccess.Read, FileShare.None);
+                var operation = await client.AnalyzeDocumentAsync(WaitUntil.Completed, "prebuilt-read", fileStream, new AnalyzeDocumentOptions { Locale = "bg" }, cancellationToken);
                 var result = operation.Value;
+
+                fileStream.Seek(0, SeekOrigin.Begin);
+                var image = SKImage.FromEncodedData(fileStream);
+
+                // TODO: Drawe a rectangle around all incorrect words.
 
                 foreach (var word in result.Pages.SelectMany(x => x.Words))
                 {
-                    if (!this._wordsRegister.Contains(word.Content))
-                    {
-                        incorrectWords.Add(word);
-                    }
+                    var sanitizedContent = SanitizeWord(word.Content);
+                    if (!this._wordsRegister.Contains(sanitizedContent)) incorrectWords.Add(word);
                 }
             }
 
             return this.Ok(incorrectWords.Select(x => x.Content));
         }
+
+        private async Task<string> SaveImageAsync(string dataDirectory, string traceIdentifier, IFormFile formFile, CancellationToken cancellationToken)
+        {
+            var pathToFolder = Path.Combine(AppContext.BaseDirectory, dataDirectory, traceIdentifier.Replace(':', '_'));
+            if (!Directory.Exists(pathToFolder)) Directory.CreateDirectory(pathToFolder);
+
+            var fullPath = Path.Combine(pathToFolder, formFile.FileName);
+            await using var fileStream = new FileStream(fullPath, FileMode.CreateNew, FileAccess.Write, FileShare.None);
+
+            await formFile.CopyToAsync(fileStream, cancellationToken);
+
+            return fullPath;
+        }
+
+        private string SanitizeWord(string originalWord) => originalWord.Trim(_delimiters);
     }
 }
